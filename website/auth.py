c2538cbd45ccb377f0fd502145b7b06b90268dcd
cwill83247 <breakthrumusic@hotmail.com>
2022-10-03 12:04:59 +0000

    Add: Getting Inital python pages ot diaplay HTML so can see working

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint   #std import so we can create blueprints
 
 auth = Blueprint('auth', __name__)   #std this is a load of routes inside to point to our relevant files
+
+@auth.route('/login')               #std defining route for login and what function it calls
+def login():                        #std  function that gets called when we visit the /login url coudl be called whatever but cleaner if stick to page name 
+    return "<h1>should return when login page visited</h1>"
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logout page</h1>" 
+
+@auth.route('/sign_up')
+def sign_up():
+    return "<h1>SignUp Page</h1>"         
--- website/views.py
+++ website/views.py
@@ -5,4 +5,4 @@ views = Blueprint('views', __name__)                    # std this is a load of 
 @views.route('/')                                       # std so defining what forward slash / does in this case calls the home function
 
 def home():
-    return "<h1>Testing the route is working</h1>"      # test to make sure its working- we also need to define the route on the __init__.py file 
+    return "<h1>Testing the route is working for Home Page</h1>"      # std test to make sure its working- we also need to define the route on the __init__.py file 
